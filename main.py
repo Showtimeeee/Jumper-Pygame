e6579131680ada32f544717380f4ab7394866db0
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2023-04-17 15:47:16 +0300

    sounds, screens, fix scores

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame as pg
 import random
 import os
+from pygame import mixer
 
+
+mixer.init()
 pg.init()
 
 WIDTH = 500
@@ -13,6 +16,15 @@ pg.display.set_caption('Jumper')
 # game fps
 clock = pg.time.Clock()
 fps = 60
+
+# sound
+pg.mixer.music.load('sound/8BitDreamscapeTheWholeOther.mp3')
+pg.mixer.music.set_volume(0.5)
+pg.mixer.music.play(-1, 0.0)
+jump_sound = pg.mixer.Sound('sound/jumpik.mp3')
+jump_sound.set_volume(0.3)
+death_sound = pg.mixer.Sound('sound/kapelki.mp3')
+death_sound.set_volume(0.5)
 
 # scroll
 scroll_thresh = 200
@@ -56,12 +68,12 @@ player_image = pg.image.load('images/playerb5.png').convert_alpha()
 bg_image = pg.image.load('images/bgvektor.jpg').convert_alpha()
 platform_image = pg.image.load('images/wood111z.png').convert_alpha()
 
-
+# text
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
-
+# score textpanel
 def score_panel():
     pg.draw.rect(screen, purple_color, (0, 0, WIDTH, 30))
     pg.draw.line(screen, game_color, (0, 30), (WIDTH, 30), 2)
@@ -117,13 +129,8 @@ class Player:
                         dy = 0
                         self.rect.bottom = platform.rect.top
                         self.vel_y = -20
+                        jump_sound.play()
 
-        # collision floor
-        # if self.rect.bottom + dy > HEIGHT:
-        #     dy = 0
-        #     self.vel_y = -20
-
-        # if coll plyaer to floor
         if self.rect.top <= scroll_thresh:
             # if jump
             if self.vel_y < 0:
@@ -135,15 +142,11 @@ class Player:
 
         return scroll
 
-
     def draw(self):
-        # проверить
         screen.blit(pg.transform.flip(self.image, self.flip, False), (self.rect.x -3, self.rect.y - 1))
-        pg.draw.rect(screen, white_color, self.rect, 2)
 
 
 class Platform(pg.sprite.Sprite):
-
     def __init__(self, x, y, width, moving):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.transform.scale(platform_image, (width, 10))
@@ -157,6 +160,7 @@ class Platform(pg.sprite.Sprite):
 
     # update platform pos
     def update(self, scroll):
+
         # moving platform
         if self.moving == True:
             self.move_counter += 1
@@ -177,15 +181,6 @@ jumper = Player(WIDTH // 2, HEIGHT - 150)
 
 # platform sprite
 platform_group = pg.sprite.Group()
-
-# going platform
-# for pl in range(max_platforms):
-#     p_w = random.randint(40, 60)
-#     p_x = random.randint(0, WIDTH - p_w)
-#     p_y = pl * random.randint(80, 120)
-#     platform = Platform(p_x, p_y, p_w)
-#     platform_group.add(platform)
-
 platform = Platform(WIDTH // 2, HEIGHT - 50, 80, False)
 platform_group.add(platform)
 
@@ -217,16 +212,11 @@ while run:
             platform = Platform(p_x, p_y, p_w, p_moving)
             platform_group.add(platform)
 
-        # scroll white line
-        # pg.draw.line(screen, white_color, (0, scroll_thresh), (WIDTH, scroll_thresh))
-
-        # update platform
         platform_group.update(scroll)
 
         # +score
         if scroll > 0:
             score += scroll
-        print(score)
 
         # draw sprites
         platform_group.draw(screen)
@@ -238,22 +228,19 @@ while run:
         # if game over
         if jumper.rect.top > HEIGHT:
             game_over = True
-
-        print(game_over)
+            death_sound.play()
 
     else:
         draw_text('Score: ' + str(score), font_big, white_color,140, 50 )
         draw_text('Game Over', font_big, white_color, 140, 100)
-
         draw_text('Press SPACE', font_big, white_color, 120, 350 )
 
         # update records write file
         if score > hight_scrore:
             hight_scrore = score
-
+            # write file scrore
             with open('score.txt', 'w') as file:
                 file.write(str(hight_scrore))
-                print(hight_scrore)
 
         # if game over press SPACE
         key = pg.key.get_pressed()
@@ -268,7 +255,6 @@ while run:
             # add new platforms
             platform = Platform(WIDTH // 2 - 50, HEIGHT - 50, 100, 0)
             platform_group.add(platform)
-
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
